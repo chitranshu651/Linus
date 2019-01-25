50435fef9ebb15d6d9156d52875e7d296f0b836e
iosdev747 <iosdev747@icloud.com>
2019-01-26 03:30:05 +0530

    Add google Dialogflow.

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -1,11 +1,20 @@
 package core;
 
+import com.google.gson.JsonElement;
 import com.jfoenix.controls.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
 import java.lang.reflect.Field;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class Controller {
 
@@ -14,6 +23,9 @@ public class Controller {
 
     @FXML
     private JFXTextField inputField;
+
+    @FXML
+    private JFXListView list;
 
     private boolean isProcessing = false;
     private Thread voiceThread;
@@ -30,13 +42,53 @@ public class Controller {
         }
         else {
             isProcessing = !isProcessing;
-//            if(inputField.getText().equals("")) {
-//                return;
-//            }
+            if(inputField.getText().equals("")) {
+                return;
+            }
             //setting GOOGLE_APPLICATION_CREDENTIALS variable
-            setEnv("GOOGLE_APPLICATION_CREDENTIALS", "/home/iosdev747/Downloads/creds.json");
-            voiceThread = new Thread(new Recognizer());
-            voiceThread.start();
+//            setEnv("GOOGLE_APPLICATION_CREDENTIALS", "/home/iosdev747/Downloads/creds.json");
+//            voiceThread = new Thread(new Recognizer());
+//            voiceThread.start();
+            AIConfiguration configuration = new AIConfiguration("c3a31db2f9bc467abebad1e364b8ff9f");
+
+            AIDataService dataService = new AIDataService(configuration);
+            System.out.print("START ");
+            try {
+                AIRequest request = new AIRequest(inputField.getText());
+                //random unique sessionID
+                request.setSessionId("1337");
+                AIResponse response = dataService.request(request);
+                System.out.println("\nSend Request: "+inputField.getText());
+                System.out.println("session:"+response.getSessionId());
+                if (response.getStatus().getCode() == 200) {
+                    String action = response.getResult().getAction();
+                    System.out.println("Action: "+action);
+                    System.out.println("Speech: "+response.getResult().getFulfillment().getSpeech());
+                    if(!response.getResult().getFulfillment().getSpeech().isEmpty()){
+                        list.getItems().add(response.getResult().getFulfillment().getSpeech());
+                    }else{
+                        list.getItems().add(response.getResult().getAction());
+                        HashMap<String, JsonElement> hashMap = response.getResult().getParameters();
+                        Object[] arrays = hashMap.keySet().toArray();
+                        for (Object o:arrays) {
+                            System.out.println("" + hashMap.get(o));
+                            list.getItems().add(o+":"+hashMap.get(o));
+                        }
+                        if(response.getResult().getAction().equals("cda") || response.getResult().getAction().equals("cdr")) {
+                            StringJoiner joiner = new StringJoiner("/");
+                            for (Object o:hashMap.keySet().toArray()) {
+                                joiner.add(hashMap.get(o).toString()).add("/");
+                            }
+                            return;
+                        }
+                        System.out.println("output:" + response.getResult().getAction());
+                    }
+                } else {
+                    System.err.println(response.getStatus().getErrorDetails());
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             goButton.setText("Stop");
         }
     }
