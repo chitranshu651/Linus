e5716a676d67b8a45ca1499144eff21c3a86a2ed
iosdev747 <iosdev747@icloud.com>
2019-01-26 01:32:05 +0530

    Adding DAO and voice recognizer

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -11,11 +11,25 @@ public class Controller {
     @FXML
     private JFXTextField inputField;
 
-    public void action() {
-        if(inputField.getText().equals("")) {
-            return;
-        }
-        System.out.println(inputField.getText());
-    }
+    private boolean isProcessing = false;
+    private Thread voiceThread;
 
+    public void action() {
+        if(isProcessing) {
+            isProcessing = !isProcessing;
+            if(voiceThread.isAlive()) {
+                voiceThread.stop();
+            }
+            goButton.setText("Go");
+        }
+        else {
+            isProcessing = !isProcessing;
+            if(inputField.getText().equals("")) {
+                return;
+            }
+            voiceThread = new Thread(new Recognizer());
+            voiceThread.start();
+            goButton.setText("Stop");
+        }
+    }
 }
