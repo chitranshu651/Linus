038a479856331570dd5c091d1804868ed7248742
iosdev474 <iosdev474@icloud.com>
2019-01-26 01:56:43 +0530

    Added command script

---
+++ command.py
@@ -0,0 +1,12 @@
+import subprocess
+from main import parameter
+
+
+def execute_command():
+    with open("output.data", "w") as outfile, open("error.data", "w") as errfile:
+        # subprocess.call("TERM=xterm top -n 1 -b", shell=True, stdout=outfile)
+        subprocess.call(parameter['command'], shell=True, stdout=outfile, stderr=errfile)
+    with open("output.data", "r") as outfile, open("error.data", "r") as errfile:
+        if errfile.read():
+            return ":-("
+        return outfile.read()
