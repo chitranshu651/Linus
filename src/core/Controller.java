de5aada844d28b1f84fb0784c3c278215deed3b6
iosdev747 <iosdev747@icloud.com>
2019-01-26 01:59:50 +0530

    Adding method for setting environment variable for credentials.

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -3,6 +3,9 @@ package core;
 import com.jfoenix.controls.*;
 import javafx.fxml.FXML;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 public class Controller {
 
     @FXML
@@ -27,9 +30,29 @@ public class Controller {
             if(inputField.getText().equals("")) {
                 return;
             }
+            //setting GOOGLE_APPLICATION_CREDENTIALS variable
+            setEnv("GOOGLE_APPLICATION_CREDENTIALS", "/home/iosdev747/Downloads/creds.json");
             voiceThread = new Thread(new Recognizer());
             voiceThread.start();
             goButton.setText("Stop");
+        }
+    }
+
+    /**
+     * Method to set Environment Variable
+     * @param key environment variable name
+     * @param value environment variable value
+     */
+    public static void setEnv(String key, String value) {
+        try {
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put(key, value);
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
         }
     }
 }
