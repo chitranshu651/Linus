a88d89fbe70b4c259e5219e9a84e1525c0730d48
iosdev474 <iosdev474@icloud.com>
2019-01-26 03:09:28 +0530

    Add sudo command support

--- command.py
+++ command.py
@@ -1,11 +1,25 @@
+import os
 import subprocess
 
 
 def execute_command(parameter):
     with open("output.data", "w") as outfile, open("error.data", "w") as errfile:
-        # subprocess.call("TERM=xterm top -n 1 -b", shell=True, stdout=outfile)
         subprocess.call(parameter['command'], shell=True, stdout=outfile, stderr=errfile)
     with open("output.data", "r") as outfile, open("error.data", "r") as errfile:
         if errfile.read():
-            return ":-("
+            return "Error occurred"
+        return outfile.read()
+
+
+'''
+It takes sudo passoword using environment variable "password"
+'''
+def sudo_execute_command(parameter):
+    with open("output.data", "w") as outfile, open("error.data", "w") as errfile:
+        p = subprocess.Popen('sudo -S' + parameter['command'], shell=True, stdout=outfile, stderr=errfile)
+        sudo_prompt = p.communicate(os.environ['password'] + '\n')[1]
+    with open("output.data", "r") as outfile, open("error.data", "r") as errfile:
+        if errfile.read():
+            return "Error occurred"
+
         return outfile.read()
