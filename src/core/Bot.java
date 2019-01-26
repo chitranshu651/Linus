170a8e2c2c70ecc624235e82dac8c912465d05d5
iosdev747 <iosdev747@icloud.com>
2019-01-26 15:26:22 +0530

    Add watchdog on error, for automatic error searching.

--- src/core/Bot.java
+++ src/core/Bot.java
@@ -1,22 +1,22 @@
 package core;
 
+import core.watchdogs.ErrorWatchDog;
 import core.watchdogs.OutputWatchDog;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
-public class Bot implements Runnable, Initializable {
+public class Bot implements Runnable {
 
     Bot() { (new Thread(this)).start(); }
 
     @Override
     public void run() {
-
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+        DAO.pwd = Paths.get(".");
+        System.out.println(DAO.pwd.toString());
         new OutputWatchDog();
+        new ErrorWatchDog();
     }
 }
