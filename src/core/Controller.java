4f44e5e12b1d8d3c2ccfa3ac12af6989ba9facca
iosdev747 <iosdev747@icloud.com>
2019-01-26 15:29:04 +0530

    Improve GUI and it's logic

--- src/core/Controller.java
+++ src/core/Controller.java
@@ -8,35 +8,32 @@ import com.google.gson.JsonElement;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+import javax.management.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.Field;
+import java.net.*;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     private Label ipLabel;
 
     @FXML
     private Label pwdLabel;
-
-    @FXML
-    private Label cpuLabel;
-
-    @FXML
-    private Label ramLabel;
 
     @FXML
     private JFXButton goButton;
@@ -53,14 +50,14 @@ public class Controller {
     @FXML
     public void onGoAction(ActionEvent actionEvent) {
         System.out.println("action Running");
-        if(isProcessing) {
+        if (isProcessing) {
             isProcessing = !isProcessing;
-            if(voiceThread.isAlive()) {
+            if (voiceThread.isAlive()) {
                 voiceThread.stop();
             }
-            if(inputField.getText().equals("")) {
+            if (inputField.getText().equals("")) {
                 inputField.setText(DAO.voiceOutput);
-                if(inputField.getText().equals("")) {
+                if (inputField.getText().equals("")) {
                     return;
                 }
             }
@@ -73,30 +70,31 @@ public class Controller {
                 //random unique sessionID
                 request.setSessionId("1337");
                 AIResponse response = dataService.request(request);
-                System.out.println("\nSend Request: "+inputField.getText());
-                System.out.println("session:"+response.getSessionId());
+                System.out.println("\nSend Request: " + inputField.getText());
+                System.out.println("session:" + response.getSessionId());
                 if (response.getStatus().getCode() == 200) {
                     String action = response.getResult().getAction();
-                    System.out.println("Action: "+action);
-                    System.out.println("Speech: "+response.getResult().getFulfillment().getSpeech());
-                    if(!response.getResult().getFulfillment().getSpeech().isEmpty()){
-                        list.getItems().add(response.getResult().getFulfillment().getSpeech());
-                    }else{
-                        list.getItems().add(response.getResult().getAction());
+                    System.out.println("Action: " + action);
+                    System.out.println("Speech: " + response.getResult().getFulfillment().getSpeech());
+                    if (!response.getResult().getFulfillment().getSpeech().isEmpty()) {
+//                        list.getItems().add(response.getResult().getFulfillment().getSpeech());
+                    } else {
+//                        list.getItems().add(response.getResult().getAction());
                         HashMap<String, JsonElement> hashMap = response.getResult().getParameters();
                         Object[] arrays = hashMap.keySet().toArray();
-                        for (Object o:arrays) {
+                        for (Object o : arrays) {
                             System.out.println("" + hashMap.get(o));
-                            list.getItems().add(o+":"+hashMap.get(o));
+//                            list.getItems().add(o+":"+hashMap.get(o));
                         }
                         //if action equals to "cda" or "cdr" change pwd variable
-                        if(response.getResult().getAction().equals("cda") || response.getResult().getAction().equals("cdr")) {
+                        if (response.getResult().getAction().equals("cda") || response.getResult().getAction().equals("cdr")) {
                             StringJoiner joiner = new StringJoiner("/");
-                            for (Object o:hashMap.keySet().toArray()) {
+                            for (Object o : hashMap.keySet().toArray()) {
                                 joiner.add(hashMap.get(o).toString()).add("/");
                             }
                             //generate pwd from output from dialogflow
-                            DAO.pwd = Paths.get(DAO.pwd.toString().concat(joiner.toString()));
+                            DAO.pwd = Paths.get(DAO.pwd.toString().concat(joiner.toString())).toAbsolutePath().normalize();
+                            pwdLabel.setText("Current Dir : " + DAO.pwd.toString());
                             return;
                         }
                         System.out.println("output command:" + response.getResult().getAction());
@@ -109,8 +107,7 @@ public class Controller {
                 ex.printStackTrace();
             }
             goButton.setText("Go");
-        }
-        else {
+        } else {
             isProcessing = !isProcessing;
             //setting GOOGLE_APPLICATION_CREDENTIALS variable
             setEnv("GOOGLE_APPLICATION_CREDENTIALS", "/home/iosdev747/Downloads/creds.json");
@@ -122,25 +119,28 @@ public class Controller {
 
     /**
      * Method to run python command which is connected through dialogflow.
-     * @param action action to be taken
+     *
+     * @param action  action to be taken
      * @param hashMap parameters for taking that action
      */
     public void runPython(String action, HashMap<String, JsonElement> hashMap) {
-        StringBuilder str = new StringBuilder("python "+DAO.pythonPath+" " + action + " ");
-        for (Object o:hashMap.keySet().toArray()) {
-            str.append(o).append(":").append(hashMap.get(o).toString().replace(' ', '#')).append(",");
+        StringBuilder str = new StringBuilder("python " + DAO.pythonPath + "/main.py " + action + " ");
+        for (Object o : hashMap.keySet().toArray()) {
+            str.append(o).append(":").append(hashMap.get(o).toString().replace(' ', '#').replace("\"", "")).append(",");
         }
-        str.append("pwd").append(":").append(DAO.pwd.toString());
+        System.out.println(DAO.pwd.toString());
+        str.append("pwd").append(":").append(DAO.pwd.toAbsolutePath().toString());
         /*
         sample output
         String str = "python /home/abcd/test/main.py act1 Name1:Value1,Name2:Value2,Name3:Value3,pwd:path";
          */
+        System.out.println("Running command:" + str.toString());
         try {
             String command = str.toString();
             Process proc = Runtime.getRuntime().exec(command);
             BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line = "";
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.print(line + "\n");
             }
             try {
@@ -155,7 +155,8 @@ public class Controller {
 
     /**
      * Method to set Environment Variable
-     * @param key environment variable name
+     *
+     * @param key   environment variable name
      * @param value environment variable value
      */
     public static void setEnv(String key, String value) {
@@ -172,6 +173,7 @@ public class Controller {
     }
 
     public void updateGUI() {
+        list.setOpacity(1);
         System.out.println("updating GUI");
         VBox vBox = new VBox();
         Output output = DAO.output;
@@ -180,23 +182,23 @@ public class Controller {
         Iterator textIterator = output.Text.iterator();
         String name = output.getTitle();
         list.getItems().add(name);
-        for (int i:DAO.output.getOrder()) {
-            if(i == Constant.IMAGE) {
+        for (int i : DAO.output.getOrder()) {
+            if (i == Constant.IMAGE) {
                 System.out.println("image");
-                core.template.Image image = ((core.template.Image)imageIterator.next());
+                core.template.Image image = ((core.template.Image) imageIterator.next());
                 javafx.scene.image.Image sceneimage = new javafx.scene.image.Image(image.getSrc());
                 ImageView imageView = new ImageView(sceneimage);
-                if(sceneimage.getWidth() >= Constant.MAXWIDTH || sceneimage.getHeight() >= Constant.MAXHEIGHT) {
+                if (sceneimage.getWidth() >= Constant.MAXWIDTH || sceneimage.getHeight() >= Constant.MAXHEIGHT) {
                     sceneimage = new javafx.scene.image.Image(image.getSrc(), Constant.MAXWIDTH, Constant.MAXHEIGHT, true, false);
                     imageView = new ImageView(sceneimage);
                 }
                 vBox.getChildren().add(imageView);
-            } else if(i == Constant.URL) {
+            } else if (i == Constant.URL) {
                 System.out.println("url");
-                vBox.getChildren().add(new Label(((core.template.URI)uriIterator.next()).getUrl()));
-            } else if(i == Constant.TEXT) {
+                vBox.getChildren().add(new Label(((core.template.URI) uriIterator.next()).getUrl()));
+            } else if (i == Constant.TEXT) {
                 System.out.println("text");
-                vBox.getChildren().add(new Label(((core.template.Text)textIterator.next()).getData()));
+                vBox.getChildren().add(new Label(((core.template.Text) textIterator.next()).getData()));
             }
         }
         list.getItems().add(vBox);
@@ -207,4 +209,22 @@ public class Controller {
         System.out.println(err);
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try (final DatagramSocket socket = new DatagramSocket()) {
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ipLabel.setText("IP : " + socket.getLocalAddress().getHostAddress());
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        pwdLabel.setText("Current Dir : " + DAO.pwd.toAbsolutePath().normalize().toString());
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        list.setOpacity(0);
+    }
 }
