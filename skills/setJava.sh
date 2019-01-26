dbf836c473267f887512b188b7c79d70f3e41847
Chitranshu Srivastava <38110415+chitranshu651@users.noreply.github.com>
2019-01-26 05:49:45 +0530

    Easier environment change
    replaced some useless commands and made it more simplified

--- skills/setJava.sh
+++ skills/setJava.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-c="ls /usr/lib/jvm | grep java"
-ere=$(eval $c)
-
-var=$(echo $ere | rev | cut -d " " -f1 | rev)
-echo $var
-konsole -e  "sudo archlinux-java set $var"
+#Pass two arguements 1st is version and 2nd is blank or open
+n="java-$1-$2jdk"
+konsole -e  "sudo archlinux-java set $n"
